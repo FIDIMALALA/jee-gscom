edf68bb35240952163b112c425031a05bce0a40f
FIC <ifidimalala@bocasay.com>
2020-10-30 15:54:18 +0300

    First commit

---
+++ src/main/java/com/llb/jeegscom/JeeGscomApplication.java
@@ -0,0 +1,13 @@
+package com.llb.jeegscom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeGscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeGscomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llb/jeegscom/JeeGscomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llb.jeegscom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeGscomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
